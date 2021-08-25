a71d2d81eec3df91dc12fcf1949bfce531b6e14b
JAYMONSECURITY <76411871+JAYMONSECURITY@users.noreply.github.com>
2021-08-26 00:21:00 +0200

    Add files via upload

---
+++ graficas/conex.php
@@ -0,0 +1,22 @@
+﻿<?php  
+    
+session_start(); #Inicia una conexion o reanuda una exixtente
+    
+$servername = "localhost";   #Localhost o IP
+    
+$username = "root";          #Usuario de la dB
+    
+$password = "";   #Contraseña de la dB
+    
+$database = "elenatfg";       #Nombre de la db
+    
+$port = "3306";              #puerto por el que se conecta la dB
+    
+$conn = mysqli_connect($servername, $username, $password, $database, $port);
+        
+if (!$conn) {
+        
+die("Conexion no establecida: " . mysqli_connect_error());
+        
+}
+   
---
+++ graficas/face2face.php
@@ -0,0 +1,122 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Fake News USA</title>
+
+		<style type="text/css">
+#container {
+  height: 400px; 
+}
+
+.highcharts-figure, .highcharts-data-table table {
+  min-width: 310px; 
+  max-width: 800px;
+  margin: 1em auto;
+}
+
+.highcharts-data-table table {
+  font-family: Verdana, sans-serif;
+  border-collapse: collapse;
+  border: 1px solid #EBEBEB;
+  margin: 10px auto;
+  text-align: center;
+  width: 100%;
+  max-width: 500px;
+}
+.highcharts-data-table caption {
+  padding: 1em 0;
+  font-size: 1.2em;
+  color: #555;
+}
+.highcharts-data-table th {
+  font-weight: 600;
+  padding: 0.5em;
+}
+.highcharts-data-table td, .highcharts-data-table th, .highcharts-data-table caption {
+  padding: 0.5em;
+}
+.highcharts-data-table thead tr, .highcharts-data-table tr:nth-child(even) {
+  background: #f8f8f8;
+}
+.highcharts-data-table tr:hover {
+  background: #f1f7ff;
+}
+
+		</style>
+	</head>
+	<body>
+<script src="code/highcharts.js"></script>
+<script src="code/highcharts-3d.js"></script>
+<script src="code/modules/exporting.js"></script>
+<script src="code/modules/export-data.js"></script>
+<script src="code/modules/accessibility.js"></script>
+
+<figure class="highcharts-figure">
+    <div id="container"></div>
+    <p class="highcharts-description">
+        Se define el grafico referente a las fake news en elecciones en USA.
+    </p>
+</figure>
+
+<?php
+include ("conex.php");
+$query = "SELECT CANDIDATO, COUNT(*) AS TOTAL FROM `bdatos` WHERE DONDE IN ('Discurso', 'Reunion', 'Mitin', 'Debate', 'Entrevista') group by candidato order by TOTAL desc";
+  
+$result = mysqli_query($conn, $query);
+ 
+?>
+
+
+		<script type="text/javascript">
+Highcharts.chart('container', {
+    chart: {
+        type: 'pie',
+        options3d: {
+            enabled: true,
+            alpha: 45,
+            beta: 0
+        }
+    },
+    title: {
+        text: 'Mentiras cara a cara en discursos, entrevistas, reuniones y debates.'
+    },
+    accessibility: {
+        point: {
+            valueSuffix: '%'
+        }
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            depth: 35,
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}'
+            }
+        }
+    },
+    series: [{
+        type: 'pie',
+        name: 'Mentiras',
+        data: [
+
+        <?php
+			while ($row = mysqli_fetch_array($result)) { 
+				echo "['".$row['CANDIDATO']."',".$row['TOTAL']."],";
+
+			  }    
+
+	    ?>
+
+        ]
+    }]
+});
+		</script>
+	</body>
+</html>
